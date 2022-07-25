0625f10c9bd751cc959a045fdc61319f21336e13
Игорь Тер-Погосян <igor-526@yandex.ru>
2022-07-26 02:20:19 +0300

    Added shoplist

--- main.py
+++ main.py
@@ -18,10 +18,14 @@ def cookbook():
 def menu(choise):
     while choise != 'q':
         print('1. Показать cookbook')
+        print('2. Рассчитать список для покупок')
         print('q. Выход из программы')
         choise = input('Выберите пункт: ')
         if choise == '1':
             print(cookbook())
+            print()
+        elif choise == '2':
+            print(shoplist(input("Введите через запятую с пробелом наименования блюд: ").split(', '), int(input("Введите количество персон: "))))
             print()
         else:
             if choise != 'q':
@@ -29,4 +33,20 @@ def menu(choise):
                 print()
     else:
         print('До свидания!')
+def shoplist(dishes, person):
+    cook_book = cookbook()
+    shop_list = {}
+    for dish in dishes:
+        if dish in cook_book:
+            for ing_info in cook_book[dish]:
+                if ing_info['ingredient_name'] in shop_list:
+                    shop_list[ing_info['ingredient_name']]['quantity'] += ing_info['quantity']
+                else:
+                    shop_list[ing_info['ingredient_name']] = {'measure': ing_info['measure'], 'quantity': ing_info['quantity']}
+
+        else:
+            print(f'Ошибка! Блюда {dish} не существует в cookbook')
+    for ingredient in shop_list:
+        shop_list[ingredient]['quantity'] = shop_list[ingredient]['quantity'] * person
+    return(shop_list)
 menu(0)
